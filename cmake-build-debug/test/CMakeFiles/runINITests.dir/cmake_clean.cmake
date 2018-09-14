7960edc0efd5769ceeb53e1f31a42ea222fc309a
Nick22ll <39570178+Nick22ll@users.noreply.github.com>
2018-09-15 01:31:29 +0200

    Add files via upload

---
+++ cmake-build-debug/test/CMakeFiles/runINITests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runINITests.dir/runAllTests.cpp.o"
+  "CMakeFiles/runINITests.dir/INITest.cpp.o"
+  "runINITests.pdb"
+  "runINITests"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runINITests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
